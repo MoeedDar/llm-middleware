38ba763d028fb481d86e0a650491846eb124c311
MoeedDar <moeed.dar@gmail.com>
2023-07-09 05:08:03 +0100

    write header

--- auth.go
+++ auth.go
@@ -20,6 +20,11 @@ func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		token := r.Header.Get("Authorization")
 		sub, auth := auth(token)
 		if !auth {
