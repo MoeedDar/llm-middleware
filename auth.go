7eaa38b08e151cd2e0d7f2f455f64d390bef1f8d
MoeedDar <moeed.dar@gmail.com>
2023-07-09 05:02:41 +0100

    changed headers

--- auth.go
+++ auth.go
@@ -16,6 +16,10 @@ const (
 
 func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 		token := r.Header.Get("Authorization")
 		sub, auth := auth(token)
 		if !auth {
